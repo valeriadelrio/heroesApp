d347a9e122600489af408105691e5d96cee28bec
valeriadelrio <valeriadlrio@gmail.com>
2017-08-30 11:28:10 -0300

    HTTP DELETE . Eliminación de registros

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroes: any[]=[];
+
+  constructor(private _heroesService:HeroesService) {
+    this._heroesService.getHeroes()
+      .subscribe(data => {
+        console.log(data);
+        this.heroes=data;
+      })
+
+  }
 
   ngOnInit() {
+  }
+
+  borraHeroe(key$:string){
+    this._heroesService.borrarHeroe(key$)
+      .subscribe(respuesta=>{
+        if(respuesta){
+          console.error(respuesta);
+        }else{
+          delete this.heroes[key$];
+        }
+         console.log(respuesta);
+      })
+
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -48,4 +48,17 @@ export class HeroesService {
             .map(res=>res.json());
   }
 
+  getHeroes(){
+
+    return this.http.get(this.heroesURL)
+            .map(res=>res.json());
+  }
+
+  borrarHeroe(key$:string){
+    let url = `${this.heroeURL}/${key$}.json`;
+    return this.http.delete(url)
+      .map(res => res.json());
+
+  }
+
 }
