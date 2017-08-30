52227bdcae7c882bb06ca89b7e5d4cf0dce697a6
valeriadelrio <valeriadlrio@gmail.com>
2017-08-30 08:32:49 -0300

    HTTP - POST Creación de un nuevo registro en Firebase

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -15,13 +17,19 @@ export class HeroeComponent implements OnInit {
     casa:"Marvel"
   }
 
-  constructor() { }
+  constructor( private _heroesService: HeroesService,
+               private router: Router) { }
 
   ngOnInit() {
   }
 
   guardar(){
     console.log(this.heroe);
+    this._heroesService.nuevoHeroe(this.heroe)
+        .subscribe( data =>{
+          this.router.navigate(['/heroe', data.name])
+        },
+        error=>console.error(error));
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Heroe } from '../interfaces/heroe.interface';
+import 'rxjs/Rx';
 
 @Injectable()
 export class HeroesService {
 
-  constructor() { }
+  heroesURL:string= "https://heroesapp-95727.firebaseio.com/heroes.json"
+
+  constructor( private http: Http) { }
+
+  nuevoHeroe(heroe: Heroe){
+
+    let body = JSON.stringify(heroe);
+    let headers = new Headers({
+      'Content-Type':'application/json'
+    });
+
+    return this.http.post(this.heroesURL, body, { headers })
+            .map( res => {
+              console.log(res.json());
+              return res.json();
+            })
+
+  }
 
 }
